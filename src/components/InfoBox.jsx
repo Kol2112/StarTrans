eb239286f315f796c879b44243e765bc9611bc75
Kamil <kolarz.kamil21@gmail.com>
2025-10-17 15:26:39 +0200

    Add :hover to InfoBox style

--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -1,7 +1,7 @@
 import '../styles/infoBox.scss';
 export default function InfoBox({img='#', desc, width, height, shadow}){
     return(
-        <section style={{width: width, height: height, border:'solid white 2px', boxShadow: shadow}} className=' container :before'>
+        <section style={{width: width, height: height, boxShadow: shadow}} className=' container'>
             <img src={img} alt="ddd" />
             <p>{desc}</p>
         </section>
