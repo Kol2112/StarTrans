f680d02a8ff64f252cf82358b2f1cffc94645b2c
Kol2112 <kolarz.kamil21@gmail.com>
2025-11-09 17:27:37 +0100

    Poprawki w stylach

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../styles/Footer.css'
 export default function Footer(){
     const {t} = useTranslation();
     return(
-        <footer className='flex flexAround'>
+        <footer>
             <p>{t('footerRights')}</p>
             <p>Leżajsk, ulica</p>
             <div className='contact'>
